172273d099c1d648ee8787aceb18b5c56012f47b
OlhaVeselovska <90180548+OlhaVeselovska@users.noreply.github.com>
2021-09-26 22:32:08 +0300

    Revert "Add files via upload"

--- q.4.py
+++
@@ -1,9 +0,0 @@
-#Calculate N! (factorial)
-
-def factorial(n):
-    if n<=0:
-        return 1 #Numbers must be positive
-    else:
-        return n * factorial(n-1)
-n = int(input('Enter a positive number:'))
-print(factorial(n))
