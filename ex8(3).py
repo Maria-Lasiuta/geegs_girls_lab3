3393dd44e7e3dc790ebf474f13d59dc2b1aad60d
OlenaKalitsinska <90180782+OlenaKalitsinska@users.noreply.github.com>
2021-09-27 09:13:11 +0300

    Add files via upload

--- ex8(3).py
+++ ex8(3).py
@@ -2,15 +2,15 @@
 #6 months. Write a function returning his savings after N months.
 #(N is an input from the user)
 
-def saving_s(months):
+def saving_s(months):#створюємо функцію
   n=0
   for x in range(months):#створюємо цикл
     if months//6:#якщо число місяців кратне 6
-        n+=500 #додаємо до n 500                
-        months-=6 #кожного разу віднімаємо,6
-        #щоб порахувати вкладення за некратні шести місяців               
+        n+=500 #+500                
+        months-=6 #кожного разу віднімаємо 6,щоб порахувати
+        #вкладення за некратні шести місяці повертаючись до else              
     else:
         n+=100 # +100 за кожні не кратні шести місяці 
   return n
-c=int(input())
+c=int(input('Enter the number of months'))
 print(saving_s(c),"saved for {0} months".format(c))
